36d5d7719faadacc01c923e66773bec5cb937921
Aksenod <31983840+Aksenod@users.noreply.github.com>
2021-01-20 21:15:18 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"600871c8daacc2001e8f88f4",
+				"600871c8daacc2001e8f88f6"
+			]
+		},
+		"600871c8daacc2001e8f88f4": {
+			"id": "600871c8daacc2001e8f88f4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"600871c8daacc2001e8f88f6": {
+			"id": "600871c8daacc2001e8f88f6",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
